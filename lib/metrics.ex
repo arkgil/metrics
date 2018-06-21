6c60b5cee14dae7529f1424011d067fd343fdc95
Arkadiusz Gil <arkadiusz@arkgil.net>
2018-06-21 23:40:49 +0200

    Implement simple gauge view

--- lib/metrics.ex
+++ lib/metrics.ex
@@ -3,19 +3,26 @@ defmodule Metrics do
   Application for gathering, aggregating and reporting metrics.
   """
 
-  alias Metrics.ViewStore
+  alias Metrics.{ViewStore, Gauge}
 
   @type metric :: [atom | String.t() | number]
   @type measurement :: number
   @type view :: [atom | String.t() | number]
+  @type view_type :: :gauge
 
   @doc """
   Records a single measurement of a metric
-
-  Raises an error if given metric doesn't exist.
   """
   @spec record(metric, measurement) :: :ok
   def record(metric, measurement) when is_list(metric) and is_number(measurement) do
     ViewStore.dispatch(metric, measurement)
+  end
+
+  @doc """
+  Adds a new view of the given metric
+  """
+  @spec add_view(metric, view, view_type) :: :ok | {:error, :already_exists}
+  def add_view(metric, view, :gauge) do
+    Gauge.register(metric, view)
   end
 end
